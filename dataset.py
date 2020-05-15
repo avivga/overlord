c40c57f61d41e6207af5712dc52f17e3e7901d4c
avivga <avivga@gmail.com>
2020-05-15 14:03:57 +0300

    support celeba variants

--- dataset.py
+++ dataset.py
@@ -6,7 +6,9 @@ import imageio
 import numpy as np
 import scipy.io
 import cv2
+from tqdm import tqdm
 
+import face_alignment
 from dataset_pascal import Pascal
 
 
@@ -133,8 +135,196 @@ class Pascal3D(DataSet):
 		}
 
 
+class CelebA(DataSet):
+
+	def __init__(self, base_dir, extras):
+		super().__init__(base_dir, extras)
+
+		parser = argparse.ArgumentParser()
+		parser.add_argument('-cs', '--crop-size', type=int, nargs=2, default=(128, 128))
+		parser.add_argument('-ts', '--target-size', type=int, nargs=2, default=(128, 128))
+		parser.add_argument('-ni', '--n-images', type=int, required=False)
+
+		args = parser.parse_args(extras)
+		self.__dict__.update(vars(args))
+
+		self.__imgs_dir = os.path.join(self._base_dir, 'Img', 'img_align_celeba_png.7z', 'img_align_celeba_png')
+		self.__identity_map_path = os.path.join(self._base_dir, 'Anno', 'identity_CelebA.txt')
+		self.__attribute_map_path = os.path.join(self._base_dir, 'Anno', 'list_attr_celeba.txt')
+
+	def __list_imgs(self):
+		with open(self.__identity_map_path, 'r') as fd:
+			lines = fd.read().splitlines()
+
+		img_paths = []
+		identities = []
+
+		for line in lines:
+			img_name, identity = line.split(' ')
+			img_path = os.path.join(self.__imgs_dir, os.path.splitext(img_name)[0] + '.png')
+
+			img_paths.append(img_path)
+			identities.append(identity)
+
+		if self.n_images:
+			idx = np.random.choice(len(img_paths), size=self.n_images)
+			return np.array(img_paths)[idx].tolist(), np.array(identities)[idx].tolist()
+
+		else:
+			return img_paths, identities
+
+	def __list_attributes(self):
+		with open(self.__attribute_map_path, 'r') as fd:
+			lines = fd.read().splitlines()[2:]
+
+		attributes = dict()
+		for line in lines:
+			tokens = line.split()
+			img_name = os.path.splitext(tokens[0])[0]
+			img_attributes = np.array(list(map(int, tokens[1:])))
+			img_attributes[img_attributes == -1] = 0
+			attributes[img_name] = img_attributes
+
+		return attributes
+
+	def read(self):
+		img_paths, identity_ids = self.__list_imgs()
+		attritbute_map = self.__list_attributes()
+
+		unique_identities = list(set(identity_ids))
+
+		imgs = np.empty(shape=(len(img_paths), self.target_size[0], self.target_size[1], 3), dtype=np.uint8)
+		identities = np.empty(shape=(len(img_paths), ), dtype=np.int32)
+		attributes = np.empty(shape=(len(img_paths), 40), dtype=np.int8)
+		landmarks = np.zeros(shape=(len(img_paths), 68*2), dtype=np.int16)
+
+		fa = face_alignment.FaceAlignment(face_alignment.LandmarksType._2D, flip_input=False)
+
+		for i in tqdm(range(len(img_paths))):
+			img_name = os.path.splitext(os.path.basename(img_paths[i]))[0]
+			img = imageio.imread(img_paths[i])
+
+			img = img[
+				(img.shape[0] // 2 - self.crop_size[0] // 2):(img.shape[0] // 2 + self.crop_size[0] // 2),
+				(img.shape[1] // 2 - self.crop_size[1] // 2):(img.shape[1] // 2 + self.crop_size[1] // 2)
+			]
+
+			imgs[i] = cv2.resize(img, dsize=tuple(self.target_size))
+			identities[i] = unique_identities.index(identity_ids[i])
+			attributes[i] = attritbute_map[img_name]
+
+			preds = fa.get_landmarks(imgs[i])
+			if preds is None or len(preds) == 0:
+				continue
+
+			landmarks[i] = preds[0].flatten().astype(np.int16)
+
+		return {
+			'img': imgs,
+			'class': identities,
+			'attributes': attributes,
+			'landmarks': landmarks
+		}
+
+
+class Beards2Glasses(DataSet):
+
+	def __init__(self, base_dir, extras):
+		super().__init__(base_dir, extras)
+
+		parser = argparse.ArgumentParser()
+		# parser.add_argument('-a', '--attributes', type=int, nargs=2, required=True)
+		parser.add_argument('-cs', '--crop-size', type=int, nargs=2, default=(128, 128))
+		parser.add_argument('-ts', '--target-size', type=int, nargs=2, default=(128, 128))
+
+		args = parser.parse_args(extras)
+		self.__dict__.update(vars(args))
+
+		self.__imgs_dir = os.path.join(self._base_dir, 'Img', 'img_align_celeba_png.7z', 'img_align_celeba_png')
+		self.__identity_map_path = os.path.join(self._base_dir, 'Anno', 'identity_CelebA.txt')
+		self.__attribute_map_path = os.path.join(self._base_dir, 'Anno', 'list_attr_celeba.txt')
+
+	def __list_imgs(self):
+		with open(self.__identity_map_path, 'r') as fd:
+			lines = fd.read().splitlines()
+
+		img_paths = []
+		identities = []
+
+		for line in lines:
+			img_name, identity = line.split(' ')
+			img_path = os.path.join(self.__imgs_dir, os.path.splitext(img_name)[0] + '.png')
+
+			img_paths.append(img_path)
+			identities.append(identity)
+
+		return img_paths, identities
+
+	def __list_attributes(self):
+		with open(self.__attribute_map_path, 'r') as fd:
+			lines = fd.read().splitlines()[2:]
+
+		attributes = dict()
+		for line in lines:
+			tokens = line.split()
+			img_name = os.path.splitext(tokens[0])[0]
+			img_attributes = np.array(list(map(int, tokens[1:])))
+			img_attributes[img_attributes == -1] = 0
+			attributes[img_name] = img_attributes
+
+		return attributes
+
+	@staticmethod
+	def male_no_5_oclock(attributes):
+		return attributes[20] == 1 and attributes[0] == 0
+
+	@staticmethod
+	def beard(attributes):
+		return attributes[22] == 1 or attributes[16] == 1 or attributes[24] == 0
+
+	@staticmethod
+	def glasses(attributes):
+		return attributes[15] == 1
+
+	def read(self):
+		img_paths, identities = self.__list_imgs()
+		attritbute_map = self.__list_attributes()
+
+		classes = np.full(shape=(len(img_paths),), fill_value=-1, dtype=np.int32)
+		for i, img_path in enumerate(img_paths):
+			img_name = os.path.splitext(os.path.basename(img_path))[0]
+			img_attributes = attritbute_map[img_name]
+
+			if self.male_no_5_oclock(img_attributes) and self.beard(img_attributes) and (not self.glasses(img_attributes)):
+				classes[i] = 0
+
+			elif self.male_no_5_oclock(img_attributes) and (not self.beard(img_attributes)) and self.glasses(img_attributes):
+				classes[i] = 1
+
+		img_paths_of_attributes = np.array(img_paths)[classes != -1].tolist()
+		classes = classes[classes != -1]
+
+		imgs = np.empty(shape=(len(img_paths_of_attributes), self.target_size[0], self.target_size[1], 3), dtype=np.uint8)
+		for i in tqdm(range(len(img_paths_of_attributes))):
+			img = imageio.imread(img_paths_of_attributes[i])
+
+			img = img[
+				(img.shape[0] // 2 - self.crop_size[0] // 2):(img.shape[0] // 2 + self.crop_size[0] // 2),
+				(img.shape[1] // 2 - self.crop_size[1] // 2):(img.shape[1] // 2 + self.crop_size[1] // 2)
+			]
+
+			imgs[i] = cv2.resize(img, dsize=tuple(self.target_size))
+
+		return {
+			'img': imgs,
+			'class': classes
+		}
+
+
 supported_datasets = {
 	'cars3d': Cars3D,
 	'cub': Cub,
-	'pascal3d': Pascal3D
+	'pascal3d': Pascal3D,
+	'celeba': CelebA,
+	'beard2glasses': Beards2Glasses
 }
