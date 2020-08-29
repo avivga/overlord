4dffd836ea8d0112cceb5ebeb3f5201377e6226d
avivga <avivga@gmail.com>
2020-08-29 11:10:51 +0300

    update config

--- config.py
+++ config.py
@@ -3,19 +3,19 @@ base_config = dict(
 	class_dim=512,
 	style_dim=64,
 
-	content_std=1,
+	content_std=0,
 
 	perceptual_loss=dict(
 		layers=[2, 7, 12, 21, 30]
 	),
 
 	style_descriptor=dict(
-		layer=7,  # 21
-		dim=2*128  # 512
+		layer=30,
+		dim=2*512
 	),
 
 	train=dict(
-		batch_size=32,
+		batch_size=16,
 		n_epochs=500,
 
 		learning_rate=dict(
@@ -26,7 +26,7 @@ base_config = dict(
 
 		loss_weights=dict(
 			reconstruction=1,
-			content_decay=0.01
+			content_decay=0.001
 		)
 	),
 
